a0e912953bd8fea70366ae41c1d95804e88565ab
Saket <saketsinghofficial404@gmail.com>
2026-01-07 22:55:38 +0530

    adding GUI to function

--- Screenshot App/main.py
+++ Screenshot App/main.py
@@ -1,11 +1,20 @@
 import time
 import pyautogui
+import tkinter as tk
 
 def screenshot():
     name = int(round(time.time()*100))
     name = '{}.png'.format(name)
-    time.sleep(5)
     img = pyautogui.screenshot(name)
     img.show()
 
-screenshot()
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame,text='Click Screenshot',command=screenshot)
+button.pack(side=tk.LEFT)
+close = tk.Button(frame,text="Exit",command=quit)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
